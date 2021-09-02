34e545e4a8d3b7259f55c8731a89da2d711c9407
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2021-09-02 18:55:55 +0200

    Try to send the log file, line by line.

--- src/example.py
+++ src/example.py
@@ -3,20 +3,31 @@ from paho.mqtt.client import ssl as mqtt_ssl
 
 from login_details import url, port, user, password
 
+# Print log file.
+file1 = open("test.log", "r")
+Lines = file1.readlines()
+count = 0
+for count, line in enumerate(Lines):
+    print("Line {}: {}".format(count + 1, line.strip())) # Strips the newline character.
+    if count >= 10:
+        break
+print("Successfully printed the logfile.")
+
 device_id = "test-device"
-
 client = mqtt.Client()
-
 client.tls_set_context(mqtt_ssl.create_default_context())
 client.username_pw_set(user, password)
 client.connect(url, port)
-
 client.tls_set_context(mqtt_ssl.create_default_context())
 
 client.loop_start()
 
-# Publish example.
-client.publish(topic="none/test-device/telemetry", payload='{"test": 42}', qos=0, retain=False)
+# Publish data
+for count, line in enumerate(Lines):
+    client.publish(topic="none/test-device/telemetry", payload=line.strip(), qos=0, retain=False)
+    if count >= 10:
+        break
 
 client.loop_stop()
+
 client.close()
