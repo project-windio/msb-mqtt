6c00b2fdddf109132a1fc1ba4df7489e38be691f
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2022-02-10 18:20:21 +0100

    Update message with ISO time stamp and different components (x y z) of acceleration.

--- src/example.py
+++ src/example.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 from paho.mqtt.client import ssl as mqtt_ssl
 import json
+import pytz
+from datetime import datetime
 
 from login_details import url, port, user, password, topic
 
@@ -37,7 +39,10 @@ def log_to_mqtt_payload(log_line, id="urn:uni-bremen:bik:wio:0:1:msb:0001"):
     """
     time = log_line.split(",")[0]
     time = time.split("[")[1]
+    time = datetime.fromtimestamp(float(time), tz=pytz.utc).isoformat()
     acc_x = log_line.split(",")[2].strip()
+    acc_y = log_line.split(",")[3].strip()
+    acc_z = log_line.split(",")[4].strip()
     dict = {
         "content-spec": "urn:spec://eclipse.org/unide/measurement-message#v3",
         "device": {
@@ -46,8 +51,14 @@ def log_to_mqtt_payload(log_line, id="urn:uni-bremen:bik:wio:0:1:msb:0001"):
         "measurements": [
             {
             "context": {
-                "acceleration in x":  {
-                "unit": "m s-2"
+                "acc_x":  {
+                    "unit": "m s-2"
+                },
+                "acc_y":  {
+                    "unit": "m s-2"
+                },
+                "acc_z":  {
+                    "unit": "m s-2"
                 }
             },
             "ts": time,
@@ -55,8 +66,14 @@ def log_to_mqtt_payload(log_line, id="urn:uni-bremen:bik:wio:0:1:msb:0001"):
                 "time": [
                 0
                 ],
-                "acceleration": [
-                acc_x
+                "acc_x": [
+                    acc_x
+                ],
+                "acc_y": [
+                    acc_y
+                ],
+                "acc_z": [
+                    acc_z
                 ]
             }
             }
@@ -72,14 +89,15 @@ file1 = open("test.log", "r")
 Lines = file1.readlines()
 count = 0
 for count, line in enumerate(Lines):
-    print("Line {}: {}".format(count + 1, line.strip())) # Strips the newline character.
+    #print("Line {}: {}".format(count + 1, line.strip())) # Strips the newline character.
     if count >= send_n_lines - 1:
         break
-print("Successfully printed the logfile.")
+#print("Successfully printed the logfile.")
 
 client = mqtt.Client()
 client.username_pw_set(user, password)
 client.connect(url, port)
+print("Successfully connected.")
 client.tls_set_context(mqtt_ssl.create_default_context())
 
 
@@ -87,8 +105,10 @@ client.tls_set_context(mqtt_ssl.create_default_context())
 client.loop_start()
 
 # Publish data
+print("topic: " + topic)
 for count, line in enumerate(Lines):
     payload = log_to_mqtt_payload(line.strip())
+    print("payload:")
     print(payload)
     client.publish(topic=topic, payload="count " + str(count) + " " + payload)
     if count >= send_n_lines - 1:
