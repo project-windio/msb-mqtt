df6247e3cc8f3d0ce7be8dba21122a7df026d310
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2023-02-09 18:01:08 +0100

    Use bash variables for times.

--- startstoploop.sh
+++ startstoploop.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo "startstoploop.sh is starting."
-for i in {1..5}
+RESTART_TIME=5 # in seconds
+WAIT_TIME_DISPLAY_MSG=0.5 # in seconds
+while true
 do
-    sleep .5 # Waits 0.5 second.
+    sleep $WAIT_TIME_DISPLAY_MSG
     echo "Running msb_mqtt.py"
     python src/msb_mqtt.py
-    sleep 5 # Waits 5 second.
+    sleep $RESTART_TIME
     echo "Terminating msb_mqtt.py"
     pkill -9 -f msb_mqtt.py.py # Kill unix process, thanks to: https://superuser.com/questions/446808/how-to-manually-stop-a-python-script-that-runs-continuously-on-linux
 done
 echo "startstoploop.sh is terminating."
-sleep .5 # Waits 0.5 second.
+sleep $WAIT_TIME_DISPLAY_MSG
