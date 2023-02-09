d2e1cf1b1a753f86e94a6d777ad5c198fde3bbd8
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2023-02-09 17:56:09 +0100

    Make clear that pkill  is a unix command (does not work in Windows).

--- startstoploop.sh
+++ startstoploop.sh
@@ -7,7 +7,7 @@ do
     python src/msb_mqtt.py
     sleep 5 # Waits 5 second.
     echo "Terminating msb_mqtt.py"
-    pkill -9 -f msb_mqtt.py.py #Thanks to: https://superuser.com/questions/446808/how-to-manually-stop-a-python-script-that-runs-continuously-on-linux
+    pkill -9 -f msb_mqtt.py.py # Kill unix process, thanks to: https://superuser.com/questions/446808/how-to-manually-stop-a-python-script-that-runs-continuously-on-linux
 done
 echo "startstoploop.sh is terminating."
 sleep .5 # Waits 0.5 second.
