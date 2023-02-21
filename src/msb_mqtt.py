10591623ca737a7e1092fd2194a75641477a7808
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2023-02-21 16:19:13 +0100

    Implement script restart within script instead of an outside bash script, improve error handling.

--- src/msb_mqtt.py
+++ src/msb_mqtt.py
@@ -3,6 +3,8 @@ from paho.mqtt.client import ssl as mqtt_ssl
 import pytz, zmq, sys, logging, pickle, json
 from datetime import datetime
 from os import path
+import time
+
 
 def create_mqtt_payload(unix_epoch=0, acc_x=0, acc_y=0, acc_z=0, id=None):
     """
@@ -76,81 +78,80 @@ def create_mqtt_payload(unix_epoch=0, acc_x=0, acc_y=0, acc_z=0, id=None):
     payload = json.dumps(dict, indent=4) 
     return payload
 
+# The  complete script is restarted in regular intervals
+restart_after_seconds = 5
 
-# Read config.
-with open(path.join("src", "msb_mqtt.json")) as json_file:
-    config = json.load(json_file)
-    print(config)
-    user = config['user']
-    password = config['password']
-    url = config['url']
-    port = config['port']
-    edge_id = config['edge_id']
-    device_id = config['device_id']
-    mqtt_topic = "ppmpv3/3/DDATA/" + edge_id + "/" + device_id
-    do_print = config['print']
-    
-client = mqtt.Client()
-print("Working with user: " + user)
-client.username_pw_set(user, password)
-client.connect(url, port=port, keepalive=60*60*12)
-print("Successfully connected.")
-client.tls_set_context(mqtt_ssl.create_default_context())
-client.loop_start()
-
-
-# Receive data using ZMQ.
-ipc_protocol = 'tcp://127.0.0.1'
-ipc_port = '5556'
-connect_to = f'{ipc_protocol}:{ipc_port}'
-print(f'Trying to bind zmq to {connect_to}')
-
-ctx = zmq.Context()
-zmq_socket = ctx.socket(zmq.SUB)
-
-try:
-    zmq_socket.connect(connect_to)
-except Exception as e:
-    logging.fatal(f'Failed to bind to zeromq socket: {e}')
-    sys.exit(-1)
-
-# Subscribe to all available data.
-zmq_socket.setsockopt(zmq.SUBSCRIBE, b'')
-
-print('Successfully bound to zeroMQ receiver socket as subscriber')
+while True:
+    elapsed_time_seconds = 0
+    start_time = datetime.now()
+    # Read config.
+    with open(path.join("src", "msb_mqtt.json")) as json_file:
+        config = json.load(json_file)
+        print(config)
+        user = config['user']
+        password = config['password']
+        url = config['url']
+        port = config['port']
+        edge_id = config['edge_id']
+        device_id = config['device_id']
+        mqtt_topic = "ppmpv3/3/DDATA/" + edge_id + "/" + device_id
+        do_print = config['print']
+        
+    client = mqtt.Client()
+    print("Working with user: " + user)
+    client.username_pw_set(user, password)
+    client.connect(url, port=port, keepalive=60*60*12)
+    print("Successfully connected.")
+    client.tls_set_context(mqtt_ssl.create_default_context())
+    client.loop_start()
 
 
-is_first_data = True
+    # Receive data using ZMQ.
+    ipc_protocol = 'tcp://127.0.0.1'
+    ipc_port = '5556'
+    connect_to = f'{ipc_protocol}:{ipc_port}'
+    print(f'Trying to bind zmq to {connect_to}')
 
-print('Trying to receive data.')
-try:
-    while True:
-        (zmq_topic, data) = zmq_socket.recv_multipart(flags=zmq.NOBLOCK)
-        zmq_topic = zmq_topic.decode('utf-8')
-        data = pickle.loads(data)
-        if is_first_data:
-            print(f'Received first data: {data}')
-            print(f'From topic: {zmq_topic}')
-            is_first_data = False
-        if zmq_topic == 'imu':
-            if do_print:
-                print(f'Will send data via MQTT: {data}')
-            payload = create_mqtt_payload(unix_epoch=data[0], acc_x=data[2], acc_y=data[3], acc_z=data[4])
-            client.publish(mqtt_topic, payload)
-        else:
-            print(f'Only topic "imu" is used, however I received data on topic: {zmq_topic}')
-        continue
-except KeyboardInterrupt:
-    client.loop_stop()
-    print('Interrupted!')
-    pass
-#except Exception as e:
-#    print(f'Failed to receive message: {e} ({topic} : {data})')
-#    continue
+    ctx = zmq.Context()
+    zmq_socket = ctx.socket(zmq.SUB)
 
+    try:
+        zmq_socket.connect(connect_to)
+    except Exception as e:
+        logging.fatal(f'Failed to bind to zeromq socket: {e}')
+        sys.exit(-1)
 
+    # Subscribe to all available data.
+    zmq_socket.setsockopt(zmq.SUBSCRIBE, b'')
+    print('Successfully bound to zeroMQ receiver socket as subscriber')
 
-
-
-
-
+    is_first_data = True
+    print('Trying to receive data.')
+    while elapsed_time_seconds < restart_after_seconds:
+        try:
+            elapsed_time_seconds = (datetime.now() - start_time).total_seconds()
+            (zmq_topic, data) = zmq_socket.recv_multipart(flags=zmq.NOBLOCK)
+            zmq_topic = zmq_topic.decode('utf-8')
+            data = pickle.loads(data)
+            if is_first_data:
+                print(f'Received first data: {data}')
+                print(f'From topic: {zmq_topic}')
+                is_first_data = False
+            if zmq_topic == 'imu':
+                if do_print:
+                    print(f'Will send data via MQTT: {data}')
+                payload = create_mqtt_payload(unix_epoch=data[0], acc_x=data[2], acc_y=data[3], acc_z=data[4])
+                client.publish(mqtt_topic, payload)
+            else:
+                print(f'Only topic "imu" is used, however I received data on topic: {zmq_topic}')
+            continue
+        except KeyboardInterrupt:
+            client.loop_stop()
+            print('Interrupted!')
+            pass
+        except Exception as e:
+            print(f'Error: {e}')
+            print("Continuing in 1 second")
+            time.sleep(1.0)
+            continue
+    continue
