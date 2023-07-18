e0ebdc5fd29a5c54ab8fefe1a4004e9f162cce5d
Andreas Haselsteiner <a.haselsteiner@uni-bremen.de>
2023-07-18 12:03:23 +0200

    Move url and port into json config.

--- src/example_with_certificate/README.md
+++ src/example_with_certificate/README.md
@@ -1,2 +1,4 @@
 # Example with a certificate
 
+BIBA Server:
+http://srv11.ikap.biba.uni-bremen.de:8086/
--- src/example_with_certificate/example_with_certificate.py
+++ src/example_with_certificate/example_with_certificate.py
@@ -17,18 +17,20 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 # Read the login data
-with open(os.path.dirname(__file__) + '/example_with_certificate_login.json') as json_file:
+with open(os.path.dirname(__file__) + '/certificate_login.json') as json_file:
     config = json.load(json_file)
     print('Working with login data:')
     print(config)
     user = config['user']
     password = config['password']
+    url = config['url']
+    port = config['port']
 
 
 client.tls_set(os.path.dirname(__file__) + "/server.pem", os.path.dirname(__file__) + "/client-cert-bikbox.pem",
                 os.path.dirname(__file__) + "/decrypted-client-key-bikbox.pem")
 client.username_pw_set(user, password)
 
-client.connect("mqtt.brementestturbine.science", 8883, 60)
+client.connect(host=url, port=port, keepalive=60)
 
 client.loop_forever()
